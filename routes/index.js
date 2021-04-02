b8c0b7d578b9d2738956081348dd9d51718084df
Karan Doshi <karansdoshi@gmail.com>
2021-04-02 22:24:54 +0530

    Added NavBar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CCI-A platform for sharing code' });
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'CCI-A platform for sharing code' });
+});
+
+router.route('/contact')
+.get(function(req, res, next)
+{
+res.render('contact', { title: 'CCI-A platform for sharing code' });
+})
+.post(function(req, res, next)
+{
+	res.render('thank', { title: 'CCI-A platform for sharing code' });
+});
+
+
 
 module.exports = router;
